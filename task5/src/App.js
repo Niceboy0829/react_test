5d498c61b41f7418c7b0a4b59b57d460f55cb5bb
Niceboy0829 <nice1qazjay@gmail.com>
2022-04-30 04:47:40 +0800

    updateD

---
+++ task5/src/App.js
@@ -0,0 +1,113 @@
+import React from "react"
+import './assets/my.css'
+import icon from './imgs/icon.png'
+
+
+const App = () => {
+    return (
+        <div className="back">
+            <div className="top">
+                <img src={icon} width = {80} height = {80}/>
+                    <div className='top_right'>
+                        <h2> Adverse effect & contraindications </h2>
+                        <div className='right'>
+                            <div><h1>546</h1></div>
+                            <div className='pillar'>
+                                <div className='pillar_f'> 
+                                </div>
+                            </div>
+                            <div className='pillars'>
+                                <div className='pillars_f'> 
+                                </div>
+                            </div>
+                            
+                            
+                        </div>
+                    </div>
+            </div>
+            <div className='bottom'>
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>  
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div> 
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>
+            <div className='column'>
+                <div className='pillar_ver'>
+                    <div className='pillars_ver'> 
+                    </div>
+                </div> 
+                <div className='pillar_vers'>
+                    <div className='pillars_vers'> 
+                    </div>
+                </div>   
+            </div>   
+            <div className='bottom_right'>
+                <h2>Silliary conditions<br />
+                    between molecules
+                    </h2>
+                    <div className='number'>
+                        <h1>125%</h1>
+                        <p> 10% </p>
+                    </div> 
+            </div>   
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ task5/src/components/Pillar.js
@@ -0,0 +1,10 @@
+import React from 'reat';
+
+const Pillar = () => {
+    return (
+        <div className='pillar'>
+            <div className='pillar_f'> 
+            </div>
+        </div>
+    )
+}
